6780622a4c0cd6f978a16a1f93a7ec6db32b0c49
jiakaiPeanut@gmail.com <jiakaiPeanut@gmail.com>
2024-10-13 20:45:15 +0800

    test #define in ll

---
+++ test02/test.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+#define LENGTH 10
+
+void compute(int*a,int*b,int c,int d){
+    for(int i=0;i<10;i++){
+            b[i]=a[i]*d+c;
+        }
+}
+
+void printArray(int *array,int length){
+    for(int i=0;i<length;i++){
+            printf("%d ",array[i]);
+        }
+}
+
+int main(){
+    int a[LENGTH]={1,2,3,4,5,6,7,8,9,0};
+    int b[LENGTH]={0};
+    int c=5,d=3;
+    compute(a,b,c,d);
+    printArray(b,LENGTH);
+    return 0;
+}
